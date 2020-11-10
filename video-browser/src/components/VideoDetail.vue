b2938ee0f4fb4badf542036d0ece4d559cccccc5
Oxi_Cuza <oxicusa@gmail.com>
2020-11-10 15:52:08 +0700

    create component video list and item

---
+++ video-browser/src/components/VideoDetail.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="video-detail">
+        <iframe src="" frameborder="0"></iframe>
+        <h1>Judul</h1>
+        <p>Detail</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VideoDetail'
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ video-browser/src/components/VideoItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <li>
+        <div class="video-img">
+            <img :src="imgURL" alt="Gambar 1">
+        </div>
+        <div class="video-desc">
+            <a href=""><h3>{{ titleVideo }}</h3></a>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'VideoItem',
+    props: ['video'],
+    computed: {
+        imgURL() {
+            return this.video.snippet.thumbnails.default.url;
+        },
+        titleVideo() {
+            return this.video.snippet.title;
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
