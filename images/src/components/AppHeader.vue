586a8aa765a73dafbc86026b2cd58584fb3c19e4
Oxi_Cuza <oxicusa@gmail.com>
2020-11-11 11:50:31 +0700

    add components AppHeader and styling with semantic ui

---
+++ images/src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="ui secondary pointing menu">
+        <a href="#" class="item active">Image Storage</a>
+        <div class="right menu">
+            <div class="ui item">
+                Logout
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
