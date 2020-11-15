0ed895d409e66995c18ecebb1a416ef812914081
Oxi_Cuza <oxicusa@gmail.com>
2020-11-15 11:36:34 +0700

    styling upload form

--- images/src/components/UploadForm.vue
+++ images/src/components/UploadForm.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        Upload Form
+    <div class="dropper"> 
+        <input type="file">
+        <span>
+            Drop image here !
+        </span>
     </div>
 </template>
 
@@ -10,6 +13,41 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+    .dropper {
+        height: 40vh;
+        width: 80vw;
+        margin: 10vh auto;
+        border: 2px dashed grey;
+        border-radius: 20px;
+        overflow: hidden;
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: all .4s;
+    }
 
+    .dropper:hover {
+        background-color: rgba(155, 155, 155, 0.1);
+    }
+
+    .dropper:hover input {
+        cursor: pointer;
+    }
+
+    .dropper:hover span {
+        font-size: 20px;
+    }
+
+    input {
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        opacity: 0;
+    }
+
+    span {
+        transition: all .4s;
+    }
 </style>
